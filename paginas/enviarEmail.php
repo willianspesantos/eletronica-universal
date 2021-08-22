5e7b855c64984138a926cb9921f30028f2f13716
Willians Santos <willianspesantos@gmail.com>
2021-08-22 16:11:07 -0300

    adiciondo arquivo enviarEmail.php e refatorando código

---
+++ paginas/enviarEmail.php
@@ -0,0 +1,36 @@
+<?php
+  //Variáveis
+  $nome = $_POST['nome'];
+  $email = $_POST['email'];
+  $cidade = $_POST['cidade'];
+  $telefone = $_POST['telefone'];
+  $mensagem = $_POST['mensagem'];
+  $data_envio = date('d/m/Y');
+  $hora_envio = date('H:i:s');
+
+  //Compo E-mail
+  $arquivo = "
+    <html>
+      <p><b>Nome: </b>$nome</p>
+      <p><b>E-mail: </b>$email</p>
+      <p><b>Cidade: </b>$cidade</p>
+      <p><b>Telefone: </b>$telefone</p>
+      <p><b>Mensagem: </b>$mensagem</p>
+      <p>Este e-mail foi enviado em <b>$data_envio</b> às <b>$hora_envio</b></p>
+    </html>
+  ";
+  
+  //Emails para quem será enviado o formulário
+  $destino = "iniciandoprojetoscompic@gmail.com";
+  $assunto = "Contato pelo Site (Orçamento)";
+
+  //Este sempre deverá existir para garantir a exibição correta dos caracteres
+  $headers  = "MIME-Version: 1.0\n";
+  $headers .= "Content-type: text/html; charset=iso-8859-1\n";
+  $headers .= "From: $nome <$email>";
+
+  //Enviar
+  mail($destino, $assunto, $arquivo, $headers);
+  
+  header("location:produtos.html");
+?>
